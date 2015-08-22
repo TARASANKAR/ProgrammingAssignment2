c0d4663521eab4142fa975cff95af7d61f2df809
TARASANKAR <tarasankar.banerjee@gmail.com>
2015-08-23 00:34:14 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,20 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to 
+## 1.set the value of the matrix
+## 2.get the value of the matrix
+## 3.set the value of the inverse of the matrix
+## 4.get the value of the inverse of the matrix
+
+
+## The second function, cacheSolve calculates the inverse of the special "matrix" created with the above function. However, 
+## it first checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache and skips 
+## the computation. Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via 
+## the setInv function.
+
+
+## first function makeCacheMatrix
 
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL		## cache for matrix inverse. set to NULL by default
@@ -18,7 +31,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## second function cacheSolve
 
 cacheSolve <- function(x, ...) {
         if(is.null(x$getInv())) {	## checking if the matrix inverse is NULL or not i.e inverse has been set in cache or not
@@ -51,7 +64,7 @@ Calculating inverse
 [2,] -0.06896552  0.1896552 -0.03448276
 [3,]  1.00000000 -0.5000000  0.00000000
 
-x$getInv()	## now getInv() method returns the matrix inverse from cache. cache  has been set in the previous step
+x$getInv()	## now getInv() method returns the matrix inverse from cache. Cache  has been set in the previous step
             [,1]       [,2]        [,3]
 [1,] -1.72413793  0.7413793  0.13793103
 [2,] -0.06896552  0.1896552 -0.03448276
@@ -70,7 +83,21 @@ solve(x$get())	## manually calculating matrix inverse. The value matches with th
 [2,] -0.06896552  0.1896552 -0.03448276
 [3,]  1.00000000 -0.5000000  0.00000000
 
+x$set(matrix(c(1, 2, 5, 35), 2, 2, byrow = TRUE))	## setting x to a different matrix
 
+x$get()		## x has been set properly
+     [,1] [,2]
+[1,]    1    2
+[2,]    5   35
 
+x$getInv()	## as x has been feshly set, the cache has been cleaned
+NULL
 
+x$setInv(solve(x$get()))	## setting the cache manually. Can be done using 'cacheSolve' method also
 
+x$getInv()	## the cache is set
+     [,1]  [,2]
+[1,]  1.4 -0.08
+[2,] -0.2  0.04
+
+## This concludes the sample usages
